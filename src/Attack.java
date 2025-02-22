15364ba14377a3bb3c030edf0505bdb5f0cfaf1d
alejandro <alegalvezlo2@gmail.com>
2025-02-22 22:57:13 +0100

    Attack Class

--- src/Attack.java
+++ src/Attack.java
@@ -1,3 +1,24 @@
 public class Attack {
+    private String name;
+    private int damage;
+    private String effect;
+
+    //Constructor
+
+    public Attack(String name, int damage, String effect){
+        this.name = name;
+        this.damage = damage;
+        this.effect = effect;
+    }
+
+    //Getters
+    public String getName() { return name;}
+    public int getdamage() { return damage;}
+    public String getEffect() { return effect;}
+
+    //Display attack details
+    public void displayAttack(){
+        System.out.println("Attack: " + name + " | Damage: " + damage + " | Effect: " + effect);
+    }
     
 }
