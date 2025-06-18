2666015f0192a22811ec5fb11e23ff697b3414f8
alejandro <alegalvezlo2@gmail.com>
2025-06-18 23:44:13 +0200

    started with window

--- src/ui/GameWindow.java
+++
@@ -1,9 +0,0 @@
-package ui;
-import javax.swing.*;
-import java.awt.*;
-
-public class GameWindow  extends JFrame{
-    public GameWindow(){
-    }
-    
-}
--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -1,31 +1,27 @@
 package ui;
 
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
+
+class MyFrame extends JFrame{
+
+    Label l;
+    Button b1, b2, b3, b4;
+
+    MyFrame(){
+        super("Battle Game");
+    }
 
 
-public class MainMenu extends JFrame {
-    public MainMenu(){
-        setTitle("Tri-Battle");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setLayout(new GridLayout(3,1));
+}
 
-        JButton startButton = new JButton("Start Game");    
-        JButton optionsButton = new JButton("Options");    
-        JButton exitButton = new JButton("Exit");
+public class MainMenu {
+    public static void main(String args[]){
+        MyFrame mf = new MyFrame();
+        mf.setVisible(true);
+        mf.setSize(800,800);
+        mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        startButton.addActionListener(e -> {
-            System.out.println("Starting game...");
-            dispose();
-        });
-        exitButton.addActionListener(e -> System.exit(0));
-
-        add(startButton);
-        add(optionsButton);
-        add(exitButton);
-
-        setVisible(true);
-    } 
+    }
+    
 }
