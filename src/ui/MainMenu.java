1254a0e5dd919b91b9575f63cacb29550ca43fbf
alejandro <alegalvezlo2@gmail.com>
2025-06-20 12:07:05 +0200

    Still need to fix stuff

--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -3,6 +3,7 @@ package ui;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.plaf.basic.BasicArrowButton;
 
 class MyFrame extends JFrame{
 
@@ -12,19 +13,31 @@ class MyFrame extends JFrame{
     MyFrame(){
         super("Battle Game");
 
+        ImageIcon backgroundIcon = new ImageIcon("img.jpeg");
+
+        JPanel backgroundPanel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+                g.drawImage(backgroundIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+
         b1 = new JButton("TRIBATTLE");
         b1.setPreferredSize(new Dimension(0,150));
-        add(b1, BorderLayout.NORTH); 
+        backgroundPanel.add(b1, BorderLayout.NORTH); 
 
         b2 = new JButton("Two");
         b2.setPreferredSize(new Dimension(0,100));
-        add(b2, BorderLayout.SOUTH);  
+        backgroundPanel.add(b2, BorderLayout.SOUTH);  
 
         b3 = new JButton("Three");
-        add(b3, BorderLayout.WEST); 
+        b3.setPreferredSize(new Dimension(150,0));
+        backgroundPanel.add(b3, BorderLayout.WEST); 
 
         b4 = new JButton("Four");  
-        add(b4, BorderLayout.EAST);   
+        b4.setPreferredSize(new Dimension(150,0));
+        backgroundPanel.add(b4, BorderLayout.EAST);   
         
         Panel p = new Panel();
         p.setLayout(new GridLayout(3,1));
@@ -33,9 +46,8 @@ class MyFrame extends JFrame{
         p.add(new Button("mon"));
         add(p, BorderLayout.CENTER);
 
+        setContentPane(backgroundPanel);
     }
-
-
 }
 
 public class MainMenu {
